7415f2aea979cdaca6fd7d4417d8f3e7aa7d425a
Ze Xuan <syltaris@gmail.com>
2019-11-16 00:17:10 +0800

    we maketh the ball

--- src/pong.rs
+++ src/pong.rs
@@ -13,6 +13,10 @@ pub const ARENA_WIDTH: f32 = 100.0;
 pub const PADDLE_HEIGHT: f32 = 16.0;
 pub const PADDLE_WIDTH: f32 = 4.0;
 
+pub const BALL_VELOCITY_X: f32 = 75.0;
+pub const BALL_VELOCITY_Y: f32 = 50.0;
+pub const BALL_RADIUS: f32 = 2.0;
+
 pub struct Pong;
 
 fn initialise_camera(world: &mut World) {
@@ -35,7 +39,9 @@ impl SimpleState for Pong {
         let sprite_sheet_handle = load_sprite_sheet(world);
         //        world.register::<Paddle>(); // configures storage for specific entity, there's a better way to do this
         // no longer needed since done in PaddleSystem, and registered via there
+        world.register::<Ball>();
 
+        initialise_ball(world, sprite_sheet_handle.clone());
         initialise_paddles(world, sprite_sheet_handle);
         initialise_camera(world);
     }
@@ -125,4 +131,33 @@ fn load_sprite_sheet(world: &mut World) -> Handle<SpriteSheet> {
         (),
         &sprite_sheet_store, // place to store data in?
     )
+}
+
+pub struct Ball {
+    pub velocity: [f32; 2], // probably, type; length
+    pub radius: f32,
+}
+
+impl Component for Ball {
+    type Storage = DenseVecStorage<Self>;
+}
+
+fn initialise_ball(world: &mut World, sprite_sheet_handle: Handle<SpriteSheet>) {
+    let mut local_transform = Transform::default();
+    local_transform.set_translation_xyz(ARENA_WIDTH / 2.0, ARENA_HEIGHT / 2.0, 0.0);
+
+    let sprite_render = SpriteRender {
+        sprite_sheet: sprite_sheet_handle,
+        sprite_number: 1,
+    };
+
+    world
+        .create_entity()
+        .with(sprite_render)
+        .with(Ball {
+            radius: BALL_RADIUS,
+            velocity: [BALL_VELOCITY_X, BALL_VELOCITY_Y],
+        })
+        .with(local_transform)
+        .build();
 }
